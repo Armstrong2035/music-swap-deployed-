036fe403cb92fcade9f5f95d5ef32bdfbbf4eafb
[Armstrong Olusoji] <115874137+Armstrong2035@users.noreply.github.com>
2024-07-04 14:36:27 +0400

    redirect uri

---
+++ .history/music-swap-frontend/src/Spotify/Authentication/spotifyCredentials_20240704143612.js
@@ -0,0 +1,21 @@
+const CLIENT_ID = "b841fe980d704720934f655f5e92d2e3";
+const REDIRECT_URI = "https://music-swap.vercel.app/login";
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+const RESPONSE_TYPE = "token";
+
+const scopes = ["playlist-read-private", "user-library-read"];
+
+export const getTokenFromUrl = () =>
+  window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      const parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+      return initial;
+    }, {});
+
+export const spotifyLogin = () =>
+  `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=${scopes.join(
+    "%20"
+  )}&response_type=${RESPONSE_TYPE}&show_dialog=${true}`;
